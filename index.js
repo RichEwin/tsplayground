ddd47dba8f6ed73e5abf868aee236b22cccf88de
richewinqred <richard.ewin-lambert@qred.com>
2023-09-26 11:21:40 +0200

    build: ts

--- index.js
+++ index.js
@@ -1,2 +1,46 @@
+// Return type
 var hello = "Hello World";
-console.log(hello);
+// Array
+var bands = [];
+bands.push("Metallica");
+var bands2 = [];
+bands2.push("Metallica");
+var myGuitarist = {
+    name: "Keith",
+    age: 66,
+    isAwesome: true,
+    albums: ["Sticky Fingers", 1971],
+};
+var myDrummer = {
+    name: "Charlie",
+    age: 80,
+    isAwesome: true,
+    albums: ["Sticky Fingers", 1971],
+};
+// Function type
+var multiply = function (a, b) { return a * b; };
+// Optional parameter
+var addAll = function (a, b, c) {
+    if (typeof c !== "undefined") {
+        return a + b + c;
+    }
+    return a + b;
+};
+// Default parameter
+var addAllNonOptional = function (a, b, c) {
+    if (c === void 0) { c = 2; }
+    return a + b + c;
+};
+var concatOrAdd = function (a, b, c) {
+    if (c === "add") {
+        return a + b;
+    }
+    return "".concat(a + b);
+};
+var myVal = concatOrAdd(2, 2, "concat");
+var todaysTransactions = {
+    Pizza: 10,
+    Books: 10,
+    Job: 10,
+};
+console.log(todaysTransactions[10]);
--- index.ts
+++ index.ts
@@ -1,2 +1,102 @@
+// Return type
 const hello: string = "Hello World";
-console.log(hello);
+
+// Array
+let bands: Array<string> = [];
+bands.push("Metallica");
+
+let bands2: string[] = [];
+bands2.push("Metallica");
+
+// Union type
+type GuitaristAlbums = Array<string | number>;
+
+// Object type
+type Guitarist = {
+  name: string;
+  age: number;
+  isAwesome: boolean;
+  albums: GuitaristAlbums;
+};
+
+const myGuitarist: Guitarist = {
+  name: "Keith",
+  age: 66,
+  isAwesome: true,
+  albums: ["Sticky Fingers", 1971],
+};
+
+interface Drummer {
+  // Interface type
+  name: string;
+  age: number;
+  isAwesome: boolean;
+}
+
+// Extends
+interface Albums extends Drummer {
+  albums: GuitaristAlbums;
+}
+
+const myDrummer: Albums = {
+  name: "Charlie",
+  age: 80,
+  isAwesome: true,
+  albums: ["Sticky Fingers", 1971],
+};
+
+// Function type
+const multiply = (a: number, b: number): number => a * b;
+
+// Optional parameter
+const addAll = (a: number, b: number, c?: number): number => {
+  if (typeof c !== "undefined") {
+    return a + b + c;
+  }
+  return a + b;
+};
+
+// Default parameter
+const addAllNonOptional = (a: number, b: number, c = 2): number => {
+  return a + b + c;
+};
+
+// Assertion
+type addOrContact = number | string;
+type addOrContactUnion = "add" | "concat";
+
+const concatOrAdd = (
+  a: number,
+  b: number,
+  c: addOrContactUnion
+): addOrContact => {
+  if (c === "add") {
+    return a + b;
+  }
+  return `${a + b}`;
+};
+
+let myVal: string = concatOrAdd(2, 2, "concat") as string;
+
+// Index signatures
+interface Transactions {
+  readonly [index: string]: number;
+  Pizza: number;
+  Books: number;
+  Job: number;
+}
+
+const todaysTransactions: Transactions = {
+  Pizza: 10,
+  Books: 10,
+  Job: 10,
+};
+
+console.log(todaysTransactions["Pizza"]);
+
+// Generics
+// Pick
+// Omit
+// Partial
+// Readonly
+// Record
